1e759f1d448c4e5d7775a6c2693739e0f23b9e9b
demouserlidia88 <lidia.molodiuc@gmail.com>
2023-11-28 23:25:31 +0200

    first commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+function getRadialGradient() {
+  const color1 = getRandomHexColor();
+  const color2 = getRandomHexColor();
+  const color3 = getRandomHexColor();
+
+  return `radial-gradient(circle, ${color1}, ${color2}, ${color3})`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButton.addEventListener('click', startGradientChange);
+stopButton.addEventListener('click', stopGradientChange);
+
+function startGradientChange() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.transition = 'background 1s ease-in-out';
+    document.body.style.background = getRadialGradient();
+  }, 2000);
+}
+
+function stopGradientChange() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+  document.body.style.transition = '';
+}
